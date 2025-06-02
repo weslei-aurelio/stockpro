9a97443410d4b38c852c418c66429bdd50671740
Gabriel do Nascimento <gabriel.nascimento@autoconf.com.br>
2025-06-02 20:15:03 -0300

    fix ajuste dropdown

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -47,9 +47,9 @@
                 <th scope="row">{{ $user->id }}</th>
                 <td>{{ $user->name }}</td>
                 <td>{{ $user->email }}</td>
-                <th scope="col">{{ $user->status?->name}}</th>
+                <th scope="col">{{ $user->status?->name ?? 'Sem status' }}</th>
                 <td>
-                    <div class="btn-group">
+                    <div class="btn-group dropup">
                         <button type="button" class="btn border-0 bg-transparent p-0" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical text-dark" viewBox="0 0 16 16">
                                 <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
@@ -78,7 +78,6 @@
         @endforeach
         </tbody>
     </table>
-
     <nav aria-label="Page navigation example" class="d-flex justify-content-center">
         <ul class="pagination">
             <li class="page-item">
