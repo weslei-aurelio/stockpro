8be11dfd5c60f2a59599a0d8d097a1e7c11f730d
Gabriel do Nascimento <gabriel.nascimento@autoconf.com.br>
2025-06-05 16:34:31 -0300

    criacao tabela e modal novo produto

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -1,113 +1,233 @@
-@extends('layouts.app')
+<div class="modal fade" id="NewProduct" tabindex="-1" role="dialog" aria-labellebdy="NewProduct" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-fullscreen-xl-down" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h2 class="modal-title">
+                    <strong>
+                        Novo Produto
+                    </strong>
+                </h2>
+                <button type="button" class="btn btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="{{ route('products.create') }}" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="brands">
+                            Descrição do produto
+                        </label>
+                        <input
+                            type="text"
+                            name="description"
+                            class="form-control"
+                            value="{{ old('description') }}"
+                            placeholder="Ex: Cerveja Amstel 350ml"
+                        >
+                    </div>
+                    <div class="mb-3">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label for="brand">
+                                    Marca do produto
+                                </label>
+                                <select name="brand" id="brand">
+                                    <option value="" selected="">Marca</option>
+{{--                                    @foreach ($brands as $brand)--}}
+{{--                                        <option value="{{ $brand->id }}"> {{$brand->name}} </option>--}}
+{{--                                    @endforeach--}}
+                                </select>
+                            </div>
+                            <div class="col-md-6">
+                                <a href="{{ route('brands.create') }}">Cadastrar marca</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <div class="row">
+                            <div class="col-md-6 text-start">
+                                <label for="categories">
+                                    Tipo de produto
+                                </label>
+                                <select name="category" id="category">
+                                    <option value="" selected="">Produto</option>
+{{--                                    @foreach ($categories as $category)--}}
+{{--                                        <option value="{{ $category->id }}"> {{$category->name}} </option>--}}
+{{--                                    @endforeach--}}
+                                </select>
+                            </div>
+                            <div class="col-md-6">
+                                <a href="{{ route('brands.create') }}">Cadastrar tipo</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="purchaseValue">
+                            Valor de compra (unidade)
+                        </label>
+                        <input
+                            id="purchaseValue"
+                            type="text"
+                            name="purchaseValue"
+                            class="form-control money"
+                            value="{{ old('purchaseValue') }}"
+                            placeholder="0,00"
+                        >
+                    </div>
+                    <div class="mb-3">
+                        <label for="salePrice">
+                            Valor de venda (unidade)
+                        </label>
+                        <input
+                            id="salePrice"
+                            type="text"
+                            name="salePrice"
+                            class="form-control money"
+                            value="{{ old('salePrice') }}"
+                            placeholder="0,00"
+                        >
+                    </div>
+                    <div class="mb-3">
+                        <label for="profitMargin">
+                            % de margem desejada (unidade)
+                        </label>
+                        <input
+                            id="profitMargin"
+                            type="text"
+                            name="profitMargin"
+                            class="form-control percent"
+                            value="{{ old('profitMargin') }}"
+                            placeholder="Ex: 25,00"
+                        >
+                    </div>
+                    <div class="mb-3">
+                        <label for="numberUnits">
+                            Quantidade de unidades
+                        </label>
+                        <input
+                            type="number"
+                            name="numberUnits"
+                            class="form-control"
+                            value="{{ old('numberUnits') }}"
+                            placeholder="Ex: 200"
+                        >
+                    </div>
+                </form>
+                <div class="modal-footer">
+                    <a href="/products" class="text-decoration-none text-dark">Cancelar</a>
+                    <button type="submit" class="btn btn-primary text-white custom-button-create">Salvar</button>
+                </div>
+            </div>
 
-@section('content')
-    <div class="container">
-        <h1>Cadastro de produto</h1>
-        <form action="{{ route('products.create') }}" method="POST">
-            @csrf
-            <div class="mb-3">
-                <label for="brands">
-                    Descrição do produto
-                </label>
-                <input 
-                    type="text"
-                    name="description"
-                    class="form-control"
-                    value="{{ old('description') }}"
-                    placeholder="Ex: Cerveja Amstel 350ml"
-                >
-            </div>
-            <div class="mb-3">
-                <div class="row">
-                    <div class="col-md-6">
-                        <label for="brand">
-                        Marca do produto
-                        </label>
-                        <select name="brand" id="brand">
-                            <option value="" selected="">Marca</option>
-                            @foreach ($brands as $brand)
-                                <option value="{{ $brand->id }}"> {{$brand->name}} </option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="col-md-6">
-                        <a href="{{ route('brands.create') }}">Cadastrar marca</a>
-                    </div>
-                </div>
-            </div>
-             <div class="mb-3">
-                <div class="row">
-                    <div class="col-md-6 text-start">
-                        <label for="categories">
-                            Tipo de produto
-                        </label>
-                        <select name="category" id="category">
-                            <option value="" selected="">Produto</option>
-                            @foreach ($categories as $category)
-                                <option value="{{ $category->id }}"> {{$category->name}} </option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="col-md-6">
-                        <a href="{{ route('brands.create') }}">Cadastrar tipo</a>
-                    </div>
-                </div>
-            </div>
-             <div class="mb-3">
-                <label for="purchaseValue">
-                    Valor de compra (unidade)
-                </label>
-                <input
-                    id="purchaseValue"
-                    type="text"
-                    name="purchaseValue"
-                    class="form-control money"
-                    value="{{ old('purchaseValue') }}"
-                    placeholder="0,00"
-                >
-            </div>
-            <div class="mb-3">
-                <label for="salePrice">
-                    Valor de venda (unidade)
-                </label>
-                <input 
-                    id="salePrice"
-                    type="text"
-                    name="salePrice"
-                    class="form-control money"
-                    value="{{ old('salePrice') }}"
-                    placeholder="0,00"
-                >
-            </div>
-            <div class="mb-3">
-                <label for="profitMargin">
-                    % de margem desejada (unidade)
-                </label>
-                <input
-                    id="profitMargin"
-                    type="text"
-                    name="profitMargin"
-                    class="form-control percent"
-                    value="{{ old('profitMargin') }}"
-                    placeholder="Ex: 25,00"
-                >
-            </div>
-             <div class="mb-3">
-                <label for="numberUnits">
-                    Quantidade de unidades
-                </label>
-                <input 
-                    type="number"
-                    name="numberUnits"
-                    class="form-control"
-                    value="{{ old('numberUnits') }}"
-                    placeholder="Ex: 200"
-                >
-            </div>
-            <button class="btn btn-primary text-white">Cadastrar produto</button>
-        </form>
-    </div>
-@endsection
+
+
+
+
+{{--<div class="container">--}}
+{{--        <h1>Cadastro de produto</h1>--}}
+{{--        <form action="{{ route('products.create') }}" method="POST">--}}
+{{--            @csrf--}}
+{{--            <div class="mb-3">--}}
+{{--                <label for="brands">--}}
+{{--                    Descrição do produto--}}
+{{--                </label>--}}
+{{--                <input--}}
+{{--                    type="text"--}}
+{{--                    name="description"--}}
+{{--                    class="form-control"--}}
+{{--                    value="{{ old('description') }}"--}}
+{{--                    placeholder="Ex: Cerveja Amstel 350ml"--}}
+{{--                >--}}
+{{--            </div>--}}
+{{--            <div class="mb-3">--}}
+{{--                <div class="row">--}}
+{{--                    <div class="col-md-6">--}}
+{{--                        <label for="brand">--}}
+{{--                        Marca do produto--}}
+{{--                        </label>--}}
+{{--                        <select name="brand" id="brand">--}}
+{{--                            <option value="" selected="">Marca</option>--}}
+{{--                            @foreach ($brands as $brand)--}}
+{{--                                <option value="{{ $brand->id }}"> {{$brand->name}} </option>--}}
+{{--                            @endforeach--}}
+{{--                        </select>--}}
+{{--                    </div>--}}
+{{--                    <div class="col-md-6">--}}
+{{--                        <a href="{{ route('brands.create') }}">Cadastrar marca</a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--             <div class="mb-3">--}}
+{{--                <div class="row">--}}
+{{--                    <div class="col-md-6 text-start">--}}
+{{--                        <label for="categories">--}}
+{{--                            Tipo de produto--}}
+{{--                        </label>--}}
+{{--                        <select name="category" id="category">--}}
+{{--                            <option value="" selected="">Produto</option>--}}
+{{--                            @foreach ($categories as $category)--}}
+{{--                                <option value="{{ $category->id }}"> {{$category->name}} </option>--}}
+{{--                            @endforeach--}}
+{{--                        </select>--}}
+{{--                    </div>--}}
+{{--                    <div class="col-md-6">--}}
+{{--                        <a href="{{ route('brands.create') }}">Cadastrar tipo</a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--             <div class="mb-3">--}}
+{{--                <label for="purchaseValue">--}}
+{{--                    Valor de compra (unidade)--}}
+{{--                </label>--}}
+{{--                <input--}}
+{{--                    id="purchaseValue"--}}
+{{--                    type="text"--}}
+{{--                    name="purchaseValue"--}}
+{{--                    class="form-control money"--}}
+{{--                    value="{{ old('purchaseValue') }}"--}}
+{{--                    placeholder="0,00"--}}
+{{--                >--}}
+{{--            </div>--}}
+{{--            <div class="mb-3">--}}
+{{--                <label for="salePrice">--}}
+{{--                    Valor de venda (unidade)--}}
+{{--                </label>--}}
+{{--                <input--}}
+{{--                    id="salePrice"--}}
+{{--                    type="text"--}}
+{{--                    name="salePrice"--}}
+{{--                    class="form-control money"--}}
+{{--                    value="{{ old('salePrice') }}"--}}
+{{--                    placeholder="0,00"--}}
+{{--                >--}}
+{{--            </div>--}}
+{{--            <div class="mb-3">--}}
+{{--                <label for="profitMargin">--}}
+{{--                    % de margem desejada (unidade)--}}
+{{--                </label>--}}
+{{--                <input--}}
+{{--                    id="profitMargin"--}}
+{{--                    type="text"--}}
+{{--                    name="profitMargin"--}}
+{{--                    class="form-control percent"--}}
+{{--                    value="{{ old('profitMargin') }}"--}}
+{{--                    placeholder="Ex: 25,00"--}}
+{{--                >--}}
+{{--            </div>--}}
+{{--             <div class="mb-3">--}}
+{{--                <label for="numberUnits">--}}
+{{--                    Quantidade de unidades--}}
+{{--                </label>--}}
+{{--                <input--}}
+{{--                    type="number"--}}
+{{--                    name="numberUnits"--}}
+{{--                    class="form-control"--}}
+{{--                    value="{{ old('numberUnits') }}"--}}
+{{--                    placeholder="Ex: 200"--}}
+{{--                >--}}
+{{--            </div>--}}
+{{--            <button class="btn btn-primary text-white">Cadastrar produto</button>--}}
+{{--        </form>--}}
+{{--    </div>--}}
 
 <script>
     document.addEventListener('DOMContentLoaded', function () {
