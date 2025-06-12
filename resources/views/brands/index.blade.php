26217adca0940b88470c3279eff15f5e33d39450
Gabriel do Nascimento <gabriel.nascimento@autoconf.com.br>
2025-06-11 22:15:39 -0300

    desenvolvimento front end pagina marcas

--- resources/views/brands/index.blade.php
+++ resources/views/brands/index.blade.php
@@ -7,7 +7,76 @@
 @endpush
 
 @section('content')
+<div class="container mt-5">
     @session('status')
+        <div class="alert alert-success text-center" role="alert">
+            {{ $value }}
+        </div>
+    @endsession
+    <div class="d-flex justify-content-between align-items-center mb-3">
+        <h1>
+            <strong>Marcas</strong>
+        </h1>
+        <a href="{{ route('brands.create') }}" class="btn btn-primary text-white">Nova marca</a>
+    </div>
+    <nav class="navbar">
+        <div class="container-fluid">
+            <form class="d-flex" role="search">
+                <input class="form-control me-2 navbar-brand" type="search" placeholder="Procurar" aria-label="Search"/>
+                <button class="btn btn-primary" type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                    </svg>
+                </button>
+            </form>
+        </div>
+    </nav>
+    <table class="table custom-table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Marca</th>
+                <th scope="col">Ação</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($brands as $brand)
+                <tr>
+                    <th scope="row">{{ $brand->id }}</th>
+                    <td>{{ $brand->name }}</td>
+                    <td>
+                        <div class="d-flex gap-2">
+                            <a class="btn btn-primary text-white btn-sm" href="#">Editar</a>
+                            <form action="#" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger btn-sm">Inativar</button>
+                            </form>
+                        </div>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    <nav aria-label="Page navigation example" class="d-flex justify-content-center">
+        <ul class="pagination">
+            <li class="page-item">
+                <a class="page-link" href="#" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                </a>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+                <a class="page-link" href="#" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</div>
+    <!-- @session('status')
         <div class="alert alert-success text-center" role="alert">
             {{ $value }}
         </div>
@@ -44,6 +113,6 @@
                 @endforeach
             </tbody>
         </table>
-    </div>
+    </div> -->
     @include('layouts.components.alert')    
 @endsection
